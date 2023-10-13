b5621eb393377ff762fe908f5e0bf51e57963cc3
Kamranabbasi9145 <kamranabbasi9145@gmail.com>
2023-10-12 19:08:08 -0700

    new code update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,50 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+ import { ApiService } from '../shared/api.service';
+import { EmployeeModel } from './header.model'; // Import your EmployeeModel
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  form: FormGroup;
+export class HeaderComponent implements OnInit {
+  formValue: FormGroup;
+  employeeModelObj: EmployeeModel = new EmployeeModel();
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      field1: ['', Validators.required],
-      field2: ['', Validators.required],
-      field3: ['', Validators.required],
-      field4: ['', Validators.required],
+  constructor(private fb: FormBuilder,
+    private api :ApiService) {
+    this.formValue = this.fb.group({
+      name: [''],
+      ojt: [''],
+      date: [''],
+      time: ['']
     });
+  }
 
-}
+  ngOnInit(): void {
+    // You can add any initialization logic here if needed
+  }
+
+  postEmployeeDetails() {
+    this.employeeModelObj.name = this.formValue.value.name;
+    this.employeeModelObj.ojt = this.formValue.value.ojt;
+    this.employeeModelObj.date = this.formValue.value.date;
+    this.employeeModelObj.time = this.formValue.value.time;
+
+    this.api.postEmployee(this.employeeModelObj)
+    .subscribe({
+      next: (res: any) => {
+        // Handle successful response
+        console.log(res);
+        alert("Employee added successfull")
+        this.formValue.reset();
+      },
+      error: (err: any) => {
+        // Handle error
+        alert("Please try again")
+      }
+    });
+    
+  }
 }
